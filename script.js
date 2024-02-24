a9d2766c47a34252a319f5a9d7badd7914ee6341
Rakesh Patel <rakesh246808@gmail.com>
2024-02-24 06:49:09 +0530

    added functionality on second page's buttons

--- script.js
+++ script.js
@@ -3,22 +3,28 @@ let firstPage = document.querySelector('.first-page');
 let secondPage = document.querySelector('.second-page');
 let backToFirstPageBtn = document.querySelector('.backToFirstPage');
 
+// let websiteIntro = document.querySelector('.website-intro');
+
 let allPlanetNames = document.querySelector('.allPlanetNames');
 
 let prevBtn = document.querySelector('#prevBtn');
 let nextBtn = document.querySelector('#nextBtn');
 
+original_url = `https://swapi.dev/api/planets/?format=json`;
+
 planetExploreBtn.addEventListener('click', () => {
     firstPage.classList.add('closeOpenFrstPage');
+    // websiteIntro.classList.add('closeOpenWebsiteIntro');
     secondPage.classList.add('openCloseScndPage');
-
+    allPlanetNames.innerHTML='';
     planetsNames(original_url);
+
 });
 
-original_url = `https://swapi.dev/api/planets/?format=json`;
 
 backToFirstPageBtn.addEventListener('click', () => {
     firstPage.classList.remove('closeOpenFrstPage');
+    // websiteIntro.classList.remove('closeOpenWebsiteIntro');
     secondPage.classList.remove('openCloseScndPage');
 });
 
@@ -83,18 +89,59 @@ async function planetsNames(original_url)
     var planetLength = planetsData.results.length;
 
     let listCreator;
-    let allAnchorTag;
 
     for(let i = 0 ; i<planetLength; i++)
     {
        listCreator = document.createElement('li');
-       allAnchorTag = document.createElement('a');
-       allAnchorTag.innerText=`${planetsData.results[i].name}`;
-       allAnchorTag.href=`${planetsData.results[i].url}`;
-       listCreator.appendChild(allAnchorTag);
+       listCreator.classList.add('planetUrl');
+       listCreator.innerText=`${planetsData.results[i].name}`;
+
+    //    allAnchorTag = document.createElement('a');
+    //    allAnchorTag.classList.add('planetUrl');
+    //    allAnchorTag.innerText=`${planetsData.results[i].name}`;
+    //    allAnchorTag.href=`${planetsData.results[i].url}`;
+    //    allAnchorTag.href=`rakesh`;
+
+    //    listCreator.appendChild(allAnchorTag);
        allPlanetNames.appendChild(listCreator);
     }
 
-    console.log(planetLength);
+    let allPlanetUrl = document.querySelectorAll('.planetUrl');
+    console.log(allPlanetUrl);
+      
+    for(let j=0;j<allPlanetUrl.length;j++)
+    {
+        allPlanetUrl[j].addEventListener("click",function(press){
+            // console.log(press.target.style.color='red');
+
+            thirdPage(press.target, planetsData.results[j].url);
+
+        });
+    }
     
+ 
 }
+
+
+//***********************[[[[[[[[[[[[[[[[[[[[[[[[[[ third Page Section  *************************]]]]]]]]]]]]]]]]]]]]
+let thirdPageSection = document.querySelector('.third-page');
+let currentPlanetName = document.querySelector('#currentPlanetName');
+
+let backpagebtn = document.querySelector('.backpagebtn');
+
+function thirdPage(thirdPageChange, currentPlanetUrl)
+{
+    thirdPageSection.classList.add('OpenClosetrdPage');
+    secondPage.classList.remove('openCloseScndPage');
+
+    currentPlanetName.innerText = thirdPageChange.innerText
+
+    console.log(currentPlanetUrl);
+}
+
+backpagebtn.addEventListener('click', () => {
+    
+    thirdPageSection.classList.remove('OpenClosetrdPage');
+    secondPage.classList.add('openCloseScndPage');
+
+})
